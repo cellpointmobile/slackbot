35d06ec1ec7aa7638b709cd10a29f045be9db9e2
Johan Thomsen <johan@cellpointmobile.com>
2017-10-12 16:19:50 +0200

    merge

--- event/event.go
+++ event/event.go
@@ -1,19 +1,39 @@
 package event
 
 import (
-	"fmt"
-	"net/http"
-	"os"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"math/rand"
+	"time"
 )
 
-const IFTTT_BASE_URL = "https://maker.ifttt.com/trigger"
+func Get_random_quote() (author string, quote string) {
+	urls := []string{
+		"https://www.brainyquote.com/quotes/keywords/coffee.html",
+		"https://www.brainyquote.com/quotes/keywords/coffee_2.html?vm=l",
+		"https://www.brainyquote.com/quotes/keywords/coffee_3.html?vm=l",
+		"https://www.brainyquote.com/quotes/keywords/coffee_4.html?vm=l",
+	}
 
-func Power_on() {
-	fmt.Println("power on event fired")
-	http.Get(IFTTT_BASE_URL + "/power_on/with/key/" + os.Getenv("IFTTT_TOKEN") )
-}
+	quotes := map[string]string{}
+	authors := []string{}
 
-func Power_off() {
-	fmt.Println("power off event fired")
-	http.Get(IFTTT_BASE_URL + "/power_off/with/key/" + os.Getenv("IFTTT_TOKEN") )
+	for _, url := range urls {
+		doc, err := goquery.NewDocument(url)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		doc.Find(".bq_list_i").Each(func(i int, sel *goquery.Selection) {
+			quote := sel.Find(".b-qt").Text()
+			author := sel.Find(".bq-aut").Text()
+			authors = append(authors, author)
+			quotes[author] = quote
+		})
+	}
+
+	rand.Seed(time.Now().Unix())
+	n := rand.Int() % len(authors)
+	return authors[n], quotes[authors[n]]
 }
---
+++ slack.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nlopes/slack"
+	"strings"
+	"./event"
+	"os"
+)
+
+func main() {
+	token := os.Getenv("SLACK_TOKEN")
+	api := slack.New(token)
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+Loop:
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received: ")
+			switch ev := msg.Data.(type) {
+			case *slack.ConnectedEvent:
+				fmt.Println("Connection counter:", ev.ConnectionCount)
+
+			case *slack.MessageEvent:
+				fmt.Printf("Message: %v\n", ev)
+				info := rtm.GetInfo()
+				prefix := fmt.Sprintf("<@%s> ", info.User.ID)
+
+				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
+					respond(rtm, ev, prefix)
+				}
+
+			case *slack.RTMError:
+				fmt.Printf("Error: %s\n", ev.Error())
+
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("Invalid credentials")
+				break Loop
+
+			default:
+				//Take no action
+			}
+		}
+	}
+}
+
+func respond(rtm *slack.RTM, msg *slack.MessageEvent, prefix string) {
+	var response string
+	text := msg.Text
+	text = strings.TrimPrefix(text, prefix)
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+
+	turnStuffOn := map[string]bool{
+		"coffee aye?": true,
+		"coffee now!":       true,
+		"i'm dying here":         true,
+		"on": true,
+	}
+	turnStuffOff := map[string]bool{
+		"please stop": true,
+		"ditch the black liquid":     true,
+		"off":   true,
+	}
+
+	randomResponses := map[string]string{
+		"die!": "Never!",
+		"milk?": "Ask <@U1JCMNPHC> to go get it..",
+	}
+
+	if turnStuffOn[text] {
+		response = "okay okay, relax dude.."
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+		event.Power_on()
+	} else if turnStuffOff[text] {
+		response = "Terminating coffee supplies!"
+		rtm.SendMessage(rtm.NewOutgoingMessage(response, msg.Channel))
+		event.Power_off()
+	} else if randomResponses[text] != "" {
+		rtm.SendMessage(rtm.NewOutgoingMessage(randomResponses[text], msg.Channel))
+	} else {
+		author, quote := event.Get_random_quote()
+		rtm.SendMessage(rtm.NewOutgoingMessage(quote + "\n\n - _" + author + "_", msg.Channel))
+	}
+}
